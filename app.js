a24ca09c05f1127ebbf03bd064da70d91672f5b1
ramayaser66 <ramayaser66@gmail.com>
2021-01-26 12:47:09 +0200

    render table

--- app.js
+++ app.js
@@ -1,71 +1,132 @@
-'use strict'; 
+'use strict';
 
 
-var carForm = document.getElementById('myForm'); 
-var clearButton = document.getElementById('clear'); 
- var dataArray = []; 
+var carForm = document.getElementById('myForm');
+var clearButton = document.getElementById('clear');
+var table = document.getElementById('myTable');
+var dataArray = [];
+//  var imgArray =[]; 
 
 
 
 
 
 
-function Form( carName, type, year){
+function Form(carName, type, year, img) {
     this.carName = carName;
     this.carType = type;
     this.yearModel = year;
-    dataArray.push(this); 
+    this.img = img;
+    this.imgArray = [];
+    dataArray.push(this);
 
 }
 
 
-Form.prototype.rnderTable = function
+Form.prototype.renderTable = function () {
+
+    var rawOne = document.createElement('tr');
+    table.appendChild(rawOne);
+    var cellOneA = document.createElement('td');
+    cellOneA.textContent = this.img;
+    rawOne.appendChild(cellOneA);
+
+
+    var cellOneB = document.createElement('td');
+    cellOneB.textContent = 'car name:' + this.carName;
+    rawOne.appendChild(cellOneB);
 
 
 
-function formData (event){
+
+
+
+}
+
+
+
+function formData(event) {
     event.preventDefault();
 
 
-    var car = event.target.carName.value; 
-    console.log(car); 
+    var car = event.target.carName.value;
+    console.log(car);
 
-    var carCategory = event.target.modelCategory.value; 
-    console.log(carCategory); 
-    var carModelYear = event.target.yearModel.value; 
-    console.log(carModelYear); 
+    var carCategory = event.target.modelCategory.value;
+    console.log(carCategory);
+    var carModelYear = event.target.yearModel.value;
+    console.log(carModelYear);
+
+
+
+   var object = new Form(car, carCategory, carModelYear);
+   object.renderTable();
+    console.log(dataArray);
+
+    saveData();
+
+}
+
+function saveData() {
+    localStorage.setItem('myForm', JSON.stringify(dataArray));
+}
+
+
+
+function data(){
+
+    for (var index = 0; index < dataArray.length; index++) {
+        var rawOne = document.createElement('tr');
+        table.appendChild(rawOne);
+        var cellOneA = document.createElement('td');
+        cellOneA.textContent = dataArray[index].img;
+        rawOne.appendChild(cellOneA);
     
+    
+        var cellOneB = document.createElement('td');
+        cellOneB.textContent = 'car name:' + dataArray[index].carName;
+        rawOne.appendChild(cellOneB);
+    
+        var cellOneC = document.createElement('td');
+        cellOneC.textContent = 'car model:' + dataArray[index].yearModel;
+        rawOne.appendChild(cellOneC);
+    
+        
+    }
+  
+}
 
 
-   new Form(car, carCategory, carModelYear ); 
-    console.log(dataArray); 
+
+function checkStorage() {
+    if (localStorage.getItem('myForm')) {
+        var storage = JSON.parse(localStorage.getItem('myForm'));
+        // call the render function 
+        data(); 
+    }
+
 
 }
 
-function saveData(){
-    localStorage.setItem('myForm', JSON.stringify(dataArray)); 
-}
 
 
 
 
-function clearData (event){
+
+
+function clearData(event) {
     event.preventDefault();
-   var storage = JSON.parse( localStorage.getItem('myForm')); 
+    localStorage.clear();
 }
 
 
 
 
 
+// saveData(); 
 
+carForm.addEventListener('submit', formData);
+clearButton.addEventListener('click', clearData);
 
-
-
-
-
-carForm.addEventListener('submit', formData); 
-clearButton.addEventListener('clear', clearData ); 
-saveData(); 
 
 
