c31ad9603bf8a512927e014b19879552ad270433
ramayaser66 <ramayaser66@gmail.com>
2021-01-26 12:14:50 +0200

    changes

--- app.js
+++ app.js
@@ -2,17 +2,56 @@
 
 
 var carForm = document.getElementById('myForm'); 
+var clearButton = document.getElementById('clear'); 
+ var dataArray = []; 
 
 
 
-function form (event){
-    event.preventDefault(); 
+
+
+
+function Form( carName, type, year){
+    this.carName = carName;
+    this.carType = type;
+    this.yearModel = year;
+    dataArray.push(this); 
+
+}
+
+
+Form.prototype.rnderTable = function
+
+
+
+function formData (event){
+    event.preventDefault();
+
 
     var car = event.target.carName.value; 
     console.log(car); 
 
-    // var carCategory = event.target.modelCategory.value; 
-    // console.log(carCategory); 
+    var carCategory = event.target.modelCategory.value; 
+    console.log(carCategory); 
+    var carModelYear = event.target.yearModel.value; 
+    console.log(carModelYear); 
+    
+
+
+   new Form(car, carCategory, carModelYear ); 
+    console.log(dataArray); 
+
+}
+
+function saveData(){
+    localStorage.setItem('myForm', JSON.stringify(dataArray)); 
+}
+
+
+
+
+function clearData (event){
+    event.preventDefault();
+   var storage = JSON.parse( localStorage.getItem('myForm')); 
 }
 
 
@@ -22,4 +61,11 @@ function form (event){
 
 
 
-carForm.addEventListener('click', form);
+
+
+
+carForm.addEventListener('submit', formData); 
+clearButton.addEventListener('clear', clearData ); 
+saveData(); 
+
+
